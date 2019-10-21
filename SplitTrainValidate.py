ac0714272586b69db7fdf9ffd5af0237b59c0f6c
Saurabh Garg <saurabhgarg@mysoc.net>
2019-10-21 10:46:06 +0800

    Updated SplitTrainValidate.py to include support for folder with different size images.

--- SplitTrainValidate.py
+++ SplitTrainValidate.py
@@ -1,22 +1,35 @@
 import glob, shutil, os
 from random import shuffle
 
-# Assume that all images are in train folder and copy given fraction of random image to validation folder.
-valRatio = 0.2
+# Size of the input image.
+INPUT_SIZE = 250
 
-catFiles = glob.glob("train/cat*")
-dogFiles = glob.glob("train/dog*")
-print("Number of training images for cat: {}".format(len(catFiles)))
-print("Number of training images for dog: {}".format(len(dogFiles)))
+# Assume that all images are in train folder and copy
+#  given fraction of random image to validation folder.
+VALIDATION_RATIO = 0.25
+
+catFiles    = glob.glob("train_{}/cat/*".format(INPUT_SIZE))
+dogFiles    = glob.glob("train_{}/dog/*".format(INPUT_SIZE))
+numCatFiles = len(catFiles)
+numDogFiles = len(dogFiles)
+
+print("Number of training images for cat: {}".format(numCatFiles))
+print("Number of training images for dog: {}".format(numDogFiles))
+
+if numCatFiles != numDogFiles:
+	print("Number of images for cats and dogs must be same.")
+	exit(1)
 
 shuffle(catFiles)
 shuffle(dogFiles)
 
-numValidationImages = int(len(catFiles) * valRatio)
-print("Moving {} images from train to validate".format(numValidationImages*2))
+os.makedirs("validate_{}/cat".format(INPUT_SIZE), exist_ok=False)
+os.makedirs("validate_{}/dog".format(INPUT_SIZE), exist_ok=False)
+
+numValidationImages = int(numCatFiles * VALIDATION_RATIO)
+print("Moving {} images from train_{} to validate_{}".format(numValidationImages*2, INPUT_SIZE, INPUT_SIZE))
 for i in range(numValidationImages):
-    catFileName = os.path.join("validate", catFiles[i].replace("train\\", ""))
-    dogFileName = os.path.join("validate", dogFiles[i].replace("train\\", ""))
-    
-    shutil.move(catFiles[i], catFileName)
-    shutil.move(dogFiles[i], dogFileName)
+	catFileName = catFiles[i].replace("train", "validate")
+	dogFileName = dogFiles[i].replace("train", "validate")
+	shutil.move(catFiles[i], catFileName)
+	shutil.move(dogFiles[i], dogFileName)
