ebd1f28647e4d38430f1963d680f462636d35b81
Saurabh Garg <saurabhgarg@mysoc.net>
2019-10-23 14:12:18 +0800

    Updated SplitTrainValidate to split data into three folders: train, validate, and test.

--- SplitTrainValidate.py
+++ SplitTrainValidate.py
@@ -1,35 +1,37 @@
-import glob, shutil, os
+import glob, shutil, os, sys
 from random import shuffle
 
-# Size of the input image.
-INPUT_SIZE = 250
+def moveImages(numImagesToMove, newFolder):
+	catFiles    = glob.glob("data/train/cat/*")
+	dogFiles    = glob.glob("data/train/dog/*")
+	numCatFiles = len(catFiles)
+	numDogFiles = len(dogFiles)
+	
+	print("Number of training images for cat: {}".format(numCatFiles))
+	print("Number of training images for dog: {}".format(numDogFiles))
+	
+	if numCatFiles != numDogFiles:
+		print("Number of images for cats and dogs must be same.")
+		exit(1)
+	
+	shuffle(catFiles)
+	shuffle(dogFiles)
+	
+	os.makedirs("data/{}/cat".format(newFolder), exist_ok=False)
+	os.makedirs("data/{}/dog".format(newFolder), exist_ok=False)
+	
+	numImagesToMove = int(numImagesToMove / 2)
+	print("Moving {} images from train to {} folder".format(numImagesToMove*2, newFolder))
+	for i in range(numImagesToMove):
+		catFileName = catFiles[i].replace("train", newFolder)
+		dogFileName = dogFiles[i].replace("train", newFolder)
+		shutil.move(catFiles[i], catFileName)
+		shutil.move(dogFiles[i], dogFileName)
 
 # Assume that all images are in train folder and copy
-#  given fraction of random image to validation folder.
-VALIDATION_RATIO = 0.25
+#  given fraction of random images to validation and test folders.
+NUM_VALIDATION_IMAGES = 25000 * 0.20
+NUM_TEST_IMAGES       = 25000 * 0.20
 
-catFiles    = glob.glob("train_{}/cat/*".format(INPUT_SIZE))
-dogFiles    = glob.glob("train_{}/dog/*".format(INPUT_SIZE))
-numCatFiles = len(catFiles)
-numDogFiles = len(dogFiles)
-
-print("Number of training images for cat: {}".format(numCatFiles))
-print("Number of training images for dog: {}".format(numDogFiles))
-
-if numCatFiles != numDogFiles:
-	print("Number of images for cats and dogs must be same.")
-	exit(1)
-
-shuffle(catFiles)
-shuffle(dogFiles)
-
-os.makedirs("validate_{}/cat".format(INPUT_SIZE), exist_ok=False)
-os.makedirs("validate_{}/dog".format(INPUT_SIZE), exist_ok=False)
-
-numValidationImages = int(numCatFiles * VALIDATION_RATIO)
-print("Moving {} images from train_{} to validate_{}".format(numValidationImages*2, INPUT_SIZE, INPUT_SIZE))
-for i in range(numValidationImages):
-	catFileName = catFiles[i].replace("train", "validate")
-	dogFileName = dogFiles[i].replace("train", "validate")
-	shutil.move(catFiles[i], catFileName)
-	shutil.move(dogFiles[i], dogFileName)
+moveImages(NUM_VALIDATION_IMAGES, "validation")
+moveImages(NUM_TEST_IMAGES, "test")
