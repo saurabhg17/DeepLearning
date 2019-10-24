4ce0c33cc1262be8908572a2b3ef18845fbb728c
Saurabh Garg <saurabhgarg@mysoc.net>
2019-10-24 11:33:52 +0800

    Improved comments.

--- SplitTrainingData.py
+++ SplitTrainingData.py
@@ -1,7 +1,9 @@
 import glob, shutil, os, sys
 from random import shuffle
 
-def moveImages(numImagesToMove, newFolder):
+# Move a number of randomly selected images from train folder 
+# to a folder with the give name.
+def moveImages(numImagesToMove, newFolderName):
 	catFiles    = glob.glob("data/train/cat/*")
 	dogFiles    = glob.glob("data/train/dog/*")
 	numCatFiles = len(catFiles)
@@ -17,19 +19,19 @@ def moveImages(numImagesToMove, newFolder):
 	shuffle(catFiles)
 	shuffle(dogFiles)
 	
-	os.makedirs("data/{}/cat".format(newFolder), exist_ok=False)
-	os.makedirs("data/{}/dog".format(newFolder), exist_ok=False)
+	os.makedirs("data/{}/cat".format(newFolderName), exist_ok=False)
+	os.makedirs("data/{}/dog".format(newFolderName), exist_ok=False)
 	
 	numImagesToMove = int(numImagesToMove / 2)
-	print("Moving {} images from train to {} folder".format(numImagesToMove*2, newFolder))
+	print("Moving {} images from train to {} folder".format(numImagesToMove*2, newFolderName))
 	for i in range(numImagesToMove):
-		catFileName = catFiles[i].replace("train", newFolder)
-		dogFileName = dogFiles[i].replace("train", newFolder)
+		catFileName = catFiles[i].replace("train", newFolderName)
+		dogFileName = dogFiles[i].replace("train", newFolderName)
 		shutil.move(catFiles[i], catFileName)
 		shutil.move(dogFiles[i], dogFileName)
 
 # Assume that all images are in train folder and copy
-#  given fraction of random images to validation and test folders.
+#  given fraction of randomly selected images to validate and test folders.
 NUM_VALIDATION_IMAGES = 25000 * 0.20
 NUM_TEST_IMAGES       = 25000 * 0.20
 
